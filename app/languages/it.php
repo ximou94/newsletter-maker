b0c10e9719ce5bee40407a0cb1d828b58683e4ea
maxime <grangier.maxime@gmail.com>
2017-12-18 15:14:39 +0100

    change variables from ET to mailjet

--- app/languages/it.php
+++ app/languages/it.php
@@ -3,7 +3,6 @@ Atomik\Translations::setMessages(array(
     'actualités' => 'NOTIZIE',
     'agenda' => 'AGENDA',
     'Lire la suite' => 'Leggi tutto...',
-    'Si vous ne visualisez pas correctement ce message, consultez-le en <a href="%%view_email_url%%">ligne.</a>' => 'Se non leggi correttamente questo messaggio, guardalo sul <a href="%%view_email_url%%">web</a>.',
-  'Pour vous d&eacute;sabonner, cliquez <a href="%%unsub_center_url%%" alias="D&eacute;sabonnement">ici</a>.' => 'Per annullare la tua iscrizione, clicca <a href="%%unsub_center_url%%" alias="D&eacute;sabonnement">qui</a>.',
-  'Pour g&eacute;rer vos abonnements, cliquez <a href="%%profile_center_url%%" alias="Mettre &agrave; jour Profil">ici</a>.' =>'Per gestire il tuo profilo, clicca <a href="%%profile_center_url%%" alias="Mettre &agrave; jour Profil">qui</a>.'
+    'Si vous ne visualisez pas correctement ce message, consultez-le en <a href="[[PERMALINK]]">ligne.</a>' => 'Se non leggi correttamente questo messaggio, guardalo sul <a href="[[PERMALINK]]">web</a>.',
+  'Pour vous d&eacute;sabonner, cliquez <a href="[[UNSUB_LINK_FR]]" alias="D&eacute;sabonnement">ici</a>.' => 'Per annullare la tua iscrizione, clicca <a href="[[UNSUB_LINK_IT]]" alias="D&eacute;sabonnement">qui</a>.',
 ));
