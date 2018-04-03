29d46e07d2c94b54f35666747d3ec88b42db8a80
maxime <grangier.maxime@gmail.com>
2018-04-03 10:03:24 +0200

    multiple changements

--- app/languages/de.php
+++ app/languages/de.php
@@ -1,5 +1,7 @@
 <?php
 Atomik\Translations::setMessages(array(
     'actualités' => 'NEUES',
-    'Lire la suite' => 'Weiterlesen...'
+				'Lire la suite' => 'Weiterlesen...',
+				'Si vous ne visualisez pas correctement ce message, consultez-le en <a href="[[PERMALINK]]">ligne.</a>' => 'Sollte Ihre E-Mail nicht korrekt angezeigt werden, klicken Sie<a href="[[PERMALINK]]"> hier.</a>',
+				'Pour vous d&eacute;sabonner, cliquez <a href="[[UNSUB_LINK_FR]]" alias="D&eacute;sabonnement">ici</a>.' => 'Profil <a href="[[UNSUB_LINK_FR]]" alias="D&eacute;sabonnement">ändern.</a>'
 ));
