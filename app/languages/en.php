d6591f5949ffc09d2b89a16f9ebcd9013ff2051a
maxime <grangier.maxime@gmail.com>
2018-06-12 16:01:39 +0200

    multiple changements

--- app/languages/en.php
+++ app/languages/en.php
@@ -3,10 +3,8 @@ Atomik\Translations::setMessages(array(
     'actualités' => 'LATEST NEWS',
     'agenda' => 'EVENTS',
     'Lire la suite' => 'Read more',
-    'Si vous ne visualisez pas correctement ce message, consultez-le en <a href="%1$s%2$sview_email_url%1$s%2$s">ligne.</a>' => 'To view this email as a web page click <a href="%1$s%2$sview_email_url%1$s%2$s">here.</a>',
-  'Pour vous d&eacute;sabonner, cliquez <a href="%1$s%2$sunsub_center_url%1$s%2$s" alias="D&eacute;sabonnement">ici</a>.' => 'Click <a href="%1$s%2$sunsub_center_url%1$s%2$s" alias="D&eacute;sabonnement">here</a> to unsubscribe.',
-  'Pour g&eacute;rer vos abonnements, cliquez <a href="%1$s%2$sprofile_center_url%1$s%2$s" alias="Mettre &agrave; jour Profil">ici</a>.' =>'Click <a href="%1$s%2$sprofile_center_url%1$s%2$s" alias="Mettre &agrave; jour Profil">here</a> to see your profile.',
-  'ABONNEZ-VOUS' => 'SUBSCRIBE'
+    'Si vous ne visualisez pas correctement ce message, consultez-le en <a href="[[PERMALINK]]">ligne.</a>' => 'To view this email as a web page click <a href="[[PERMALINK]]">here.</a>',
+  'Pour vous d&eacute;sabonner, cliquez <a href="[[UNSUB_LINK_EN]]" alias="D&eacute;sabonnement">ici</a>.' => 'Click <a href="[[UNSUB_LINK_EN]]" alias="Désabonnement" style="color: #2ba6cb; text-decoration: none;">here</a>  to unsubscribe.'
 ));
 
 
